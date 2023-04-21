510316f0881b8c04754ed0b673052d10e0ba0ada
CBBell99 <chrisbeckertonbell@gmail.com>
2023-04-21 11:01:31 -0700

    create handleFileInput function

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import { useState } from 'react'
 
 import './App.css'
+import FileInput from './components/FileInput';
 
 function App() {
+  const [files, setFiles] = useState<File[]>([]);
+
+  const handleFileInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedFiles = Array.from(e.target.files ?? []);
+    setFiles(prev => [...prev, ...selectedFiles]);
+    console.log(selectedFiles);
+  };
 
   return (
-      <div>
-        <h1>PDF Merge</h1>
-      </div>
-      
-  )
+    <div>
+      <h1>PDF Merge</h1>
+      <FileInput handleFileInput={handleFileInput} />
+    </div>
+  );
 }
 
 export default App
