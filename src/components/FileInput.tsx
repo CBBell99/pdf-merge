cdfbbe472e2de72add94a1a2c4a5e8b1939ff7a2
CBBell99 <chrisbeckertonbell@gmail.com>
2023-04-21 19:15:01 -0700

    create custom input button

--- src/components/FileInput.tsx
+++ src/components/FileInput.tsx
@@ -1,12 +1,37 @@
-interface FileInputProps {
+import { useRef } from 'react';
+import './FileInput.css';
+
+interface Props {
   handleFileInput: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-function FileInput({ handleFileInput }: FileInputProps) {
+function FileInput({ handleFileInput }: Props) {
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const handleClick = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+
+  const handleCustomButtonClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    handleClick();
+  };
+
   return (
     <div>
       <label className='file-input-label'>
-        <input accept='.pdf' type='file' multiple onChange={handleFileInput} />
+        <div className='btn' onClick={handleCustomButtonClick}>
+          <p className='btn-text'>Select Files</p>
+        </div>
+        <input
+          accept='.pdf'
+          type='file'
+          multiple
+          onChange={handleFileInput}
+          ref={fileInputRef}
+        />
       </label>
     </div>
   );
